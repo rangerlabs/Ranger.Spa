33b1a337f10eee1edb2c0332100c35828a7e5060
Nick Cromwell <nick.cromwell@rangerlabs.io>
2019-08-20 09:57:04 -0400

    Dev (#1)
    * Don't publish image on PR; add dev branch build
    
    * remove empty file
    
    * set SPA_HOST in webpack prod
    
    * don't push notifications on PR builds
    
    * update travis secure value

--- scripts/travis-docker-publish.sh
+++ scripts/travis-docker-publish.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 DOCKER_TAG=''
 
-case "$TRAVIS_BRANCH" in
-  "master")
-    DOCKER_TAG=1.0.$TRAVIS_BUILD_NUMBER
-    ;;
-  "dev")
-    DOCKER_TAG=dev
-    ;;    
-esac
+if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
+  case "$TRAVIS_BRANCH" in
+    "master")
+      DOCKER_TAG=1.0.$TRAVIS_BUILD_NUMBER
+      ;;
+    "dev")
+      DOCKER_TAG=dev
+      ;;    
+  esac
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push $DOCKER_USERNAME/ranger.spa:$DOCKER_TAG
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker push rangerlabs/ranger.spa:$DOCKER_TAG
+fi
