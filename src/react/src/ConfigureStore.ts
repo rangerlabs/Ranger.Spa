a19b91d5859f492dd95ecc023e617e6223c53054
Nick Cromwell <cromwellnick@gmail.com>
2019-09-22 10:54:13 -0400

    Redirect on refresh token renewal error

--- src/react/src/ConfigureStore.ts
+++ src/react/src/ConfigureStore.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers, ReducersMapObject } from "redux";
-import { routerMiddleware } from "connected-react-router";
+import { routerMiddleware, push } from "connected-react-router";
 import { loadUser } from "redux-oidc";
 import { ApplicationState } from "./stores/index";
 import { History } from "history";
@@ -7,10 +7,20 @@ import reducers from "./redux/reducers/index";
 import { connectRouter } from "connected-react-router";
 import logger from "redux-logger";
 import UserManager from "./services/UserManager";
+import RoutePaths from "./components/RoutePaths";
 
 export default function ConfigureStore(history: History, initialState?: ApplicationState) {
     UserManager.events.addSilentRenewError(function(error) {
         console.error("Error while renewing the access token.", error);
+        const domains = window.location.hostname.split(".");
+        if (domains.length === 3) {
+            const domain = domains[0];
+            const redirectUri = `http://${domain}.${SPA_HOST}/callback`;
+            UserManager.signinRedirect({ acr_values: "tenant:" + domain, redirect_uri: redirectUri, data: { redirectUrl: RoutePaths.Home } });
+        } else {
+            history.push("/");
+            return null;
+        }
     });
 
     const rootReducer = buildRootReducer(history, reducers);
