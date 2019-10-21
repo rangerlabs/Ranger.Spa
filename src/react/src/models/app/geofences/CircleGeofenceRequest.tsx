8814f9bcc0a1693290badba70adab69c1a1f742b
Nick Cromwell <cromwellnick@gmail.com>
2019-10-21 09:05:38 -0400

    Update geofence file names

---
+++ src/react/src/models/app/geofences/CircleGeofenceRequest.tsx
@@ -0,0 +1,22 @@
+import Geofence from './Geofence';
+import { ShapePicker } from '../../../redux/actions/GoogleMapsActions';
+import CoordinatePair from './CoordinatePair';
+
+export default class CircleGeofenceRequest implements Geofence {
+    shape: ShapePicker;
+
+    public constructor(
+        public name: string,
+        public labels: string[] = [],
+        public onEnter: boolean,
+        public onExit: boolean,
+        public enabled: boolean,
+        public description: string,
+        public integrationNames: string[] = [],
+        public coordinates: Array<CoordinatePair>,
+        public metadata: Map<string, object>,
+        public radius: number
+    ) {
+        this.shape = ShapePicker.Circle;
+    }
+}
---
+++ src/react/src/models/app/geofences/Geofence.tsx
@@ -0,0 +1,13 @@
+import { ShapePicker } from '../../../redux/actions/GoogleMapsActions';
+
+export default interface Geofence {
+    readonly shape: ShapePicker;
+    integrationNames: string[];
+    labels: string[];
+    name: string;
+    description: string;
+    onEnter: boolean;
+    onExit: boolean;
+    enabled: boolean;
+    metadata: Map<string, object>;
+}
