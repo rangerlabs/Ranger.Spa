a16f0e74564a44917dc32cdb30a693835ac7b3d1
Nick Cromwell <cromwellnick@gmail.com>
2019-10-21 09:24:39 -0400

    Update geofence file names

---
+++ src/react/src/components/app/geofences/googleMap/markers/NewPolygonGeofenceMapMarker.tsx
@@ -0,0 +1,223 @@
+import CoordinatePair from '../../../../../models/app/geofences/CoordinatePair';
+import Constants from '../../../../../theme/Constants';
+
+const MapMarkerRed = require('../../../../../../assets/map-marker-red.png');
+const MapMarkerPurple = require('../../../../../../assets/map-marker-green.png');
+const CircleFilledRed = require('../../../../../../assets/circle-filled-red.png');
+
+export default class NewPolygonGeofenceMapMarker {
+    polygonGeofence: google.maps.Polygon = undefined;
+    polygonMarker: google.maps.Marker = undefined;
+
+    polylines = new Array<google.maps.Polyline>();
+    polygonPathMarkers = new Array<google.maps.Marker>();
+
+    public constructor(
+        private map: google.maps.Map,
+        latLngArray: google.maps.LatLng[],
+        private addPolygonLatLng: (latLngArray: CoordinatePair[]) => void,
+        private openInfoWindow: () => void
+    ) {
+        if (latLngArray.length > 1) {
+            this.addPolygonLatLng(latLngArray.map(v => new CoordinatePair(v.lat(), v.lng())));
+            this.addPolygonGeofence(latLngArray);
+            this.setPolygonCenterMarker();
+        } else {
+            this.createPolyline(latLngArray[0]);
+        }
+    }
+
+    private addPolygonGeofence(latLngArray: google.maps.LatLng[]) {
+        this.polygonGeofence = new google.maps.Polygon({
+            map: this.map,
+            paths: latLngArray,
+            clickable: true,
+            editable: true,
+            draggable: true,
+            strokeColor: Constants.COLORS.PRIMARY_COLOR,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: Constants.COLORS.PRIMARY_COLOR,
+            fillOpacity: 0.3,
+        });
+        this.polygonGeofence.addListener('click', e => {
+            const valuesLength = this.polygonGeofence.getPath().getLength();
+            if (valuesLength >= 3) {
+                this.openInfoWindow();
+            }
+        });
+        this.polygonGeofence.addListener('mouseover', (e: google.maps.MouseEvent) => {
+            if (this.polygonMarker) {
+                this.polygonMarker.setIcon(MapMarkerRed);
+                this.polygonGeofence.setOptions({
+                    strokeColor: '#e53935',
+                    fillColor: '#e53935',
+                });
+            }
+        });
+        this.polygonGeofence.addListener('mouseout', (e: google.maps.MouseEvent) => {
+            if (this.polygonMarker) {
+                this.polygonMarker.setIcon(MapMarkerPurple);
+                this.polygonGeofence.setOptions({
+                    strokeColor: Constants.COLORS.PRIMARY_COLOR,
+                    fillColor: Constants.COLORS.PRIMARY_COLOR,
+                });
+            }
+        });
+
+        const polygonPath = this.polygonGeofence.getPath();
+        polygonPath.addListener('insert_at', e => {
+            this.handlePolygonChange();
+        });
+        polygonPath.addListener('remove_at', e => {
+            this.handlePolygonChange();
+        });
+        polygonPath.addListener('set_at', e => {
+            this.handlePolygonChange();
+        });
+    }
+
+    createPolyline(latLng: google.maps.LatLng): void {
+        const newPolyline = new google.maps.Polyline({
+            map: this.map,
+            strokeColor: '#e53935',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            path: [latLng],
+            clickable: true,
+        });
+        this.map.addListener('mousemove', (e: google.maps.MouseEvent) => {
+            const lastPolyline = this.polylines[this.polylines.length - 1];
+            lastPolyline.setPath([lastPolyline.getPath().getAt(0), e.latLng]);
+        });
+        newPolyline.addListener('click', (e: google.maps.MouseEvent) => {
+            this.createPolyline(e.latLng);
+        });
+
+        if (this.polylines.length === 0) {
+            this.polylines.push(newPolyline);
+        } else {
+            const lastPolyline = this.polylines[this.polylines.length - 1];
+            google.maps.event.clearInstanceListeners(lastPolyline);
+            const lastPolylinePath = lastPolyline.getPath();
+            lastPolylinePath.push(latLng);
+            this.polylines[this.polylines.length - 1].setPath(lastPolylinePath);
+            this.polylines.push(newPolyline);
+        }
+
+        this.pushManualPolygonMarker(latLng);
+    }
+
+    private clearPolygon() {
+        if (this.polygonGeofence) {
+            this.polygonGeofence.setMap(null);
+            this.polygonGeofence = undefined;
+        }
+        if (this.polygonMarker) {
+            this.polygonMarker.setMap(null);
+            this.polygonMarker = undefined;
+        }
+    }
+
+    private removePolygonEventListeners() {
+        if (this.polygonGeofence) {
+            google.maps.event.clearInstanceListeners(this.polygonGeofence);
+        }
+        if (this.polygonMarker) {
+            google.maps.event.clearInstanceListeners(this.polygonMarker);
+        }
+    }
+
+    private removePolylineEventListeners() {
+        if (this.polylines) {
+            this.polylines.forEach(pl => google.maps.event.clearInstanceListeners(pl));
+        }
+        if (this.polygonPathMarkers) {
+            this.polygonPathMarkers.forEach(pm => {
+                this.polylines.forEach(pm => google.maps.event.clearInstanceListeners(pm));
+            });
+        }
+        google.maps.event.clearListeners(this.map, 'mousemove');
+    }
+
+    private clearPolylinesAndMarkers() {
+        if (this.polylines) {
+            this.polylines.forEach(pl => {
+                pl.setMap(null);
+                pl = undefined;
+            });
+            this.polylines = undefined;
+        }
+        if (this.polygonPathMarkers) {
+            this.polygonPathMarkers.forEach(pm => {
+                pm.setMap(null);
+                pm = undefined;
+            });
+            this.polygonPathMarkers = undefined;
+        }
+    }
+
+    public destroy() {
+        this.removePolygonEventListeners();
+        this.removePolylineEventListeners();
+        this.clearPolygon();
+        this.clearPolylinesAndMarkers();
+    }
+
+    isClosedPolygon() {
+        const result = this.polygonGeofence ? true : false;
+        return result;
+    }
+
+    private handlePolygonChange() {
+        const polyPathArray = this.polygonGeofence.getPath().getArray();
+        const latLngArray = polyPathArray.map(v => new CoordinatePair(v.lat(), v.lng()));
+        this.addPolygonLatLng(latLngArray);
+        this.setPolygonCenterMarker();
+    }
+
+    private setPolygonCenterMarker() {
+        if (this.polygonMarker) {
+            this.polygonMarker.setPosition(this.getPolygonCenter());
+        } else {
+            this.polygonMarker = new google.maps.Marker({
+                map: this.map,
+                position: this.getPolygonCenter(),
+                icon: MapMarkerPurple,
+                animation: google.maps.Animation.BOUNCE,
+            });
+        }
+    }
+
+    private pushManualPolygonMarker(latLng: google.maps.LatLng) {
+        const marker = new google.maps.Marker({
+            map: this.map,
+            position: latLng,
+            icon: CircleFilledRed,
+        });
+        if (this.polylines.length === 1) {
+            marker.setClickable(true);
+            marker.addListener('click', (e: google.maps.MouseEvent) => {
+                if (this.polylines.length >= 3) {
+                    const latLngArray = this.polylines.map(pl => pl.getPath().getAt(0));
+                    this.addPolygonGeofence(latLngArray);
+                    this.setPolygonCenterMarker();
+                    this.addPolygonLatLng(latLngArray.map(v => new CoordinatePair(v.lat(), v.lng())));
+                    this.removePolylineEventListeners();
+                    this.clearPolylinesAndMarkers();
+                }
+            });
+        }
+        this.polygonPathMarkers.push(marker);
+        // this.polyline.setPath(this.polygonPath);
+    }
+
+    getPolygonCenter() {
+        var bounds = new google.maps.LatLngBounds();
+        for (let i = 0; i < this.polygonGeofence.getPath().getLength(); i++) {
+            bounds.extend(this.polygonGeofence.getPath().getAt(i));
+        }
+        console.log('Center: ' + bounds.getCenter());
+        return bounds.getCenter();
+    }
+}
---
+++ src/react/src/components/app/geofences/googleMap/markers/PolygonGeofenceMapMarker.tsx
@@ -0,0 +1,100 @@
+import Constants from '../../../../../theme/Constants';
+
+const MapMarkerPurple = require('../../../../../../assets/map-marker-green.png');
+const MapMarkerRed = require('../../../../../../assets/map-marker-red.png');
+
+export default class PolygonGeofenceMapMarker {
+    polygonGeofence: google.maps.Polygon = undefined;
+    polygonMarker: google.maps.Marker = undefined;
+
+    public constructor(
+        private map: google.maps.Map,
+        public id: string,
+        private latLngPath: Array<google.maps.LatLng>,
+        private onClick: (latLng: google.maps.LatLng, geofenceName: string) => void,
+        private drop: boolean = false
+    ) {
+        this.addPolygonGeofence();
+        this.addPolygonGeofenceClickEventListener();
+    }
+
+    getMarker() {
+        return this.polygonMarker;
+    }
+
+    addPolygonGeofence() {
+        this.polygonGeofence = new google.maps.Polygon({
+            clickable: true,
+            strokeColor: Constants.COLORS.PRIMARY_COLOR,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: Constants.COLORS.PRIMARY_COLOR,
+            fillOpacity: 0.2,
+            map: this.map,
+            paths: this.latLngPath,
+        });
+        this.polygonMarker = new google.maps.Marker({
+            map: this.map,
+            position: this.getPolygonCenter(),
+            icon: MapMarkerPurple,
+            animation: this.drop ? google.maps.Animation.DROP : null,
+        });
+    }
+
+    addPolygonGeofenceClickEventListener() {
+        this.polygonGeofence.addListener('click', (e: google.maps.MouseEvent) => {
+            const valuesLength = this.polygonGeofence.getPath().getLength();
+            if (valuesLength >= 3) {
+                google.maps.event.trigger(this.map, 'click', e);
+            }
+        });
+        this.polygonMarker.addListener('click', (e: google.maps.MouseEvent) => {
+            const valuesLength = this.polygonGeofence.getPath().getLength();
+            if (valuesLength >= 3) {
+                this.onClick(this.getPolygonCenter(), this.id);
+            }
+        });
+        this.polygonMarker.addListener('mouseover', (e: google.maps.MouseEvent) => {
+            this.polygonMarker.setIcon(MapMarkerRed);
+        });
+        this.polygonMarker.addListener('mouseout', (e: google.maps.MouseEvent) => {
+            this.polygonMarker.setIcon(MapMarkerPurple);
+        });
+        this.polygonGeofence.addListener('mousemove', (e: google.maps.MouseEvent) => {
+            google.maps.event.trigger(this.map, 'mousemove', e);
+        });
+    }
+
+    getPolygonCenter() {
+        var bounds = new google.maps.LatLngBounds();
+        for (let i = 0; i < this.polygonGeofence.getPath().getLength(); i++) {
+            bounds.extend(this.polygonGeofence.getPath().getAt(i));
+        }
+        return bounds.getCenter();
+    }
+
+    private removeEventListeners = () => {
+        if (this.polygonGeofence) {
+            google.maps.event.clearInstanceListeners(this.polygonGeofence);
+        }
+        if (this.polygonMarker) {
+            google.maps.event.clearInstanceListeners(this.polygonMarker);
+        }
+    };
+
+    private clearCircle = () => {
+        if (this.polygonGeofence) {
+            this.polygonGeofence.setMap(null);
+            this.polygonGeofence = undefined;
+        }
+        if (this.polygonMarker) {
+            this.polygonMarker.setMap(null);
+            this.polygonMarker = undefined;
+        }
+    };
+
+    public destroy = () => {
+        this.removeEventListeners();
+        this.clearCircle();
+    };
+}
