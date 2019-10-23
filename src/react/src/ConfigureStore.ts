2895a1d8d5ab15d19eede85b279669540978aa37
Nick Cromwell <cromwellnick@gmail.com>
2019-10-22 22:12:50 -0400

    Fix silent renew redirectUri

--- src/react/src/ConfigureStore.ts
+++ src/react/src/ConfigureStore.ts
@@ -1,24 +1,24 @@
-import { createStore, applyMiddleware, compose, combineReducers, ReducersMapObject } from "redux";
-import { routerMiddleware, push } from "connected-react-router";
-import { loadUser } from "redux-oidc";
-import { ApplicationState } from "./stores/index";
-import { History } from "history";
-import reducers from "./redux/reducers/index";
-import { connectRouter } from "connected-react-router";
-import logger from "redux-logger";
-import UserManager from "./services/UserManager";
-import RoutePaths from "./components/RoutePaths";
+import { createStore, applyMiddleware, compose, combineReducers, ReducersMapObject } from 'redux';
+import { routerMiddleware, push } from 'connected-react-router';
+import { loadUser } from 'redux-oidc';
+import { ApplicationState } from './stores/index';
+import { History } from 'history';
+import reducers from './redux/reducers/index';
+import { connectRouter } from 'connected-react-router';
+import logger from 'redux-logger';
+import UserManager from './services/UserManager';
+import RoutePaths from './components/RoutePaths';
 
 export default function ConfigureStore(history: History, initialState?: ApplicationState) {
     UserManager.events.addSilentRenewError(function(error) {
-        console.error("Error while renewing the access token.", error);
-        const domains = window.location.hostname.split(".");
+        console.error('Error while renewing the access token.', error);
+        const domains = window.location.hostname.split('.');
         if (domains.length === 3) {
             const domain = domains[0];
-            const redirectUri = `http://${domain}.${SPA_HOST}/callback`;
-            UserManager.signinRedirect({ acr_values: "tenant:" + domain, redirect_uri: redirectUri, data: { redirectUrl: RoutePaths.Dashboard } });
+            const redirectUri = `https://${domain}.${SPA_HOST}/callback`;
+            UserManager.signinRedirect({ acr_values: 'tenant:' + domain, redirect_uri: redirectUri, data: { redirectUrl: RoutePaths.Dashboard } });
         } else {
-            history.push("/");
+            history.push('/');
             return null;
         }
     });
@@ -30,8 +30,8 @@ export default function ConfigureStore(history: History, initialState?: Applicat
     loadUser(store, UserManager);
 
     if (module.hot) {
-        module.hot.accept("./redux/reducers/index", () => {
-            const nextRootReducer = require("./redux/reducers/index");
+        module.hot.accept('./redux/reducers/index', () => {
+            const nextRootReducer = require('./redux/reducers/index');
             store.replaceReducer(buildRootReducer(history, nextRootReducer));
         });
     }
