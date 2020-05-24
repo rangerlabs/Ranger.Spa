88592146bb1f90f6b212ef9ea5eba368d49e9913
Nick Cromwell <cromwellnick@gmail.com>
2020-05-24 17:41:25 -0400

    fix namesapce

--- src/Ranger.Spa/Program.cs
+++ src/Ranger.Spa/Program.cs
@@ -6,26 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Ranger.Logging;
+using Ranger.Monitoring.Logging;
 
-namespace Ranger.Spa {
-    public class Program {
-        public static void Main (string[] args) {
-            var config = new ConfigurationBuilder ()
-                .SetBasePath (Directory.GetCurrentDirectory ())
-                .AddJsonFile ("appsettings.json", optional : false, reloadOnChange : true)
-                .AddJsonFile ($"appsettings.{Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT")}.json", optional : false, reloadOnChange : true)
-                .AddEnvironmentVariables ()
-                .Build ();
+namespace Ranger.Spa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-            BuildHost (config["serverBindingUrl"], args).Run ();
+            BuildHost(config["serverBindingUrl"], args).Run();
         }
-        public static IWebHost BuildHost (string serverBindingUrl, string[] args) =>
-            WebHost.CreateDefaultBuilder (args)
-            .UseContentRoot (Directory.GetCurrentDirectory ())
-            .UseUrls (serverBindingUrl)
-            .UseStartup<Startup> ()
-            .UseLogging ()
-            .Build ();
+        public static IWebHost BuildHost(string serverBindingUrl, string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseUrls(serverBindingUrl)
+            .UseStartup<Startup>()
+            .UseLogging()
+            .Build();
     }
 }
