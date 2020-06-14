67f52cbefe8bac9221650f5197515d2809d253bb
Nick Cromwell <cromwellnick@gmail.com>
2020-06-14 02:25:39 -0400

    use window.location.href for redirect

--- src/react/src/components/notifier/pusherHandlers/OrganizationDomainUpdateHandler.ts
+++ src/react/src/components/notifier/pusherHandlers/OrganizationDomainUpdateHandler.ts
@@ -1,14 +1,13 @@
 import PusherNotificationModel from '../../../models/PusherNotificationModel';
 import ReduxStore from '../../../ReduxStore';
 import { DialogContent, openDialog } from '../../../redux/actions/DialogActions';
-import { push } from 'connected-react-router';
 import GlobalConfig from '../../../helpers/GlobalConfig';
 import RoutePaths from '../../RoutePaths';
 
 export default function OrganizationDomainUpdateHandler(data: PusherNotificationModel & { newDomain: string }): void {
     var openDialogAction = openDialog(
         new DialogContent(data.message, 'Domain update', 'Ok', () => {
-            push('https://' + data.newDomain + '.' + GlobalConfig.SPA_HOST + RoutePaths.Login);
+            window.location.href = 'https://' + data.newDomain + '.' + GlobalConfig.SPA_HOST + RoutePaths.Login;
         })
     );
     const store = ReduxStore.getStore();
