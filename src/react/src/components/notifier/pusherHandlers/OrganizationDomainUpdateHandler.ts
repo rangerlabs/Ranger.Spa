58e1000990c089470995a934161cac9993541e99
Nick Cromwell <cromwellnick@gmail.com>
2020-06-14 02:00:15 -0400

    redirect to login for new domain

--- src/react/src/components/notifier/pusherHandlers/OrganizationDomainUpdateHandler.ts
+++ src/react/src/components/notifier/pusherHandlers/OrganizationDomainUpdateHandler.ts
@@ -2,16 +2,17 @@ import PusherNotificationModel from '../../../models/PusherNotificationModel';
 import ReduxStore from '../../../ReduxStore';
 import UserManager from '../../../services/UserManager';
 import { DialogContent, openDialog } from '../../../redux/actions/DialogActions';
+import { push } from 'connected-react-router';
+import GlobalConfig from '../../../helpers/GlobalConfig';
+import RoutePaths from '../../RoutePaths';
 
-export default function OrganizationDomainUpdateHandler(data: PusherNotificationModel): void {
-    const oidcState = ReduxStore.getState().oidc;
-    const idTokenHint = ReduxStore.getState().oidc.user.id_token;
-
-    if (!oidcState.isLoadingUser && oidcState.user && !oidcState.user.expired) {
-        var openDialogAction = openDialog(
-            new DialogContent(data.message, 'Domain update', 'Ok', () => UserManager.signoutRedirect({ id_token_hint: idTokenHint }))
-        );
-        const store = ReduxStore.getStore();
-        store.dispatch(openDialogAction);
-    }
+export default function OrganizationDomainUpdateHandler(data: PusherNotificationModel & { newDomain: string }): void {
+    var openDialogAction = openDialog(
+        new DialogContent(data.message, 'Domain update', 'Ok', () => {
+            UserManager.removeUser();
+            push('https://' + data.newDomain + '.' + GlobalConfig.SPA_HOST + RoutePaths.Login);
+        })
+    );
+    const store = ReduxStore.getStore();
+    store.dispatch(openDialogAction);
 }
