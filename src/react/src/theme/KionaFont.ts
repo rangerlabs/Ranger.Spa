618a9d051e1006d2966c93208aff484f88845fcd
Nick Cromwell <cromwellnick@gmail.com>
2020-06-24 13:42:16 -0400

    add fonts to document

--- src/react/src/theme/KionaFont.ts
+++ src/react/src/theme/KionaFont.ts
@@ -10,12 +10,43 @@ import { KionaSemiBoldItallic } from '../../assets/fonts/Kiona-SemiBoldItallic.t
 const KionaRegularFont = new FontFace('Kiona-Regular, Kiona', KionaRegular, {
     style: 'normal',
 });
-const KionaItallicFont = new FontFace('Kiona-Itallic, Kiona', `url(${KionaItallic})`, {});
-const KionaLightFont = new FontFace('Kiona-Light, Kiona', `url(${KionaLight})`, {});
-const KionaLightItallicFont = new FontFace('Kiona-LightItallic, Kiona', KionaLightItallic, {});
-const KionaBoldFont = new FontFace('Kiona-Bold, Kiona', `url(${KionaBold})`, {});
-const KionaBoldItallicFont = new FontFace('Kiona-BoldItallic, Kiona', `url(${KionaBoldItallic})`, {});
-const KionaSemiBoldFont = new FontFace('Kiona-SemiBold, Kiona', `url(${KionaSemiBold})`, {});
-const KionaSemiBoldItallicFont = new FontFace('Kiona-SemiBold, Kiona', `url(${KionaSemiBoldItallic})`, {});
+document.fonts.add(KionaRegularFont);
 
-export default KionaRegularFont;
+const KionaItallicFont = new FontFace('Kiona-Itallic, Kiona', KionaItallic, {
+    style: 'normal',
+});
+document.fonts.add(KionaItallicFont);
+
+const KionaLightFont = new FontFace('Kiona-Light, Kiona', KionaLight, {
+    style: 'normal',
+});
+document.fonts.add(KionaLightFont);
+const KionaLightItallicFont = new FontFace('Kiona-LightItallic, Kiona', KionaLightItallic, {
+    style: 'normal',
+});
+document.fonts.add(KionaLightItallicFont);
+const KionaBoldFont = new FontFace('Kiona-Bold, Kiona', KionaBold, {
+    style: 'normal',
+});
+document.fonts.add(KionaBoldFont);
+const KionaBoldItallicFont = new FontFace('Kiona-BoldItallic, Kiona', KionaBoldItallic, {
+    style: 'normal',
+});
+document.fonts.add(KionaBoldItallicFont);
+const KionaSemiBoldFont = new FontFace('Kiona-SemiBold, Kiona', KionaSemiBold, {
+    style: 'normal',
+});
+document.fonts.add(KionaSemiBold);
+const KionaSemiBoldItallicFont = new FontFace('Kiona-SemiBold, Kiona', KionaSemiBoldItallic, {
+    style: 'normal',
+});
+document.fonts.add(KionaSemiBoldItallicFont);
+
+export { KionaRegularFont };
+export { KionaItallicFont };
+export { KionaLightFont };
+export { KionaLightItallicFont };
+export { KionaBoldFont };
+export { KionaBoldItallicFont };
+export { KionaSemiBoldFont };
+export { KionaSemiBoldItallicFont };
