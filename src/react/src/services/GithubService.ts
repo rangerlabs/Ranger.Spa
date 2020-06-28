609366aa5feec5fec7773495b924291cf3a8b95a
Nick Cromwell <cromwellnick@gmail.com>
2020-06-28 17:53:29 -0400

    update gettingstarted markdown link

--- src/react/src/services/GithubService.ts
+++ src/react/src/services/GithubService.ts
@@ -5,7 +5,7 @@ export default class GithubService {
     async getSection(section: DocumentationSectionEnum): Promise<IRestResponse<string>> {
         switch (section) {
             case DocumentationSectionEnum.GETTING_STARTED: {
-                return RestUtilities.request<string>('GET', 'https://raw.githubusercontent.com/rangerlabs/Ranger.Docs/master/GettingStarted.md');
+                return RestUtilities.request<string>('GET', 'https://raw.githubusercontent.com/rangerlabs/Ranger.Docs/master/GettingStarted/GettingStarted.md');
             }
         }
     }
