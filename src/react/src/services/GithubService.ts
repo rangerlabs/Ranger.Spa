226cd4ef27b29f173d49364a627c76e1cb34f383
Nick Cromwell <cromwellnick@gmail.com>
2020-06-28 19:39:09 -0400

    GithubService update

--- src/react/src/services/GithubService.ts
+++ src/react/src/services/GithubService.ts
@@ -3,9 +3,29 @@ import { DocumentationSectionEnum } from '../models/landing/DocumentationSection
 
 export default class GithubService {
     async getSection(section: DocumentationSectionEnum): Promise<IRestResponse<string>> {
+        let statusCode = 0;
+        let isError = false;
         switch (section) {
             case DocumentationSectionEnum.GETTING_STARTED: {
-                return RestUtilities.request<string>('GET', 'https://raw.githubusercontent.com/rangerlabs/Ranger.Docs/master/GettingStarted/GettingStarted.md');
+                return fetch('https://raw.githubusercontent.com/rangerlabs/Ranger.Docs/master/GettingStarted/GettingStarted.md', {
+                    method: 'GET',
+                    mode: 'cors',
+                })
+                    .then((response: Response) => {
+                        isError = response.status === 304 || (response.status >= 400 && response.status <= 500) ? true : false;
+                        statusCode = response.status;
+                        return response.text();
+                    })
+                    .then((responseContent: string) => {
+                        const responseContentJson = responseContent ? JSON.parse(responseContent) : undefined;
+                        let response: IRestResponse<string> = {
+                            statusCode: statusCode,
+                            message: responseContentJson.message,
+                            isError: isError,
+                            result: isError ? undefined : responseContentJson.result,
+                        };
+                        return response;
+                    });
             }
         }
     }
