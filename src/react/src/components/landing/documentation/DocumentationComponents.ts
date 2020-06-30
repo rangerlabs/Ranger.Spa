0524bc64e424ec1166a2ac407e5cf7578596fadf
Nick Cromwell <cromwellnick@gmail.com>
2020-06-30 17:43:29 -0400

    Add default path

--- src/react/src/components/landing/documentation/DocumentationComponents.ts
+++ src/react/src/components/landing/documentation/DocumentationComponents.ts
@@ -1,5 +1,7 @@
-import RoutePaths from '../../RoutePaths';
 import GettingStarted from './GettingStarted';
 import IRoute from '../../ILandingRoute';
 
-export const DocComponents: IRoute[] = [{ exact: true, path: `/getting-started`, component: GettingStarted }];
+export const DocComponents: IRoute[] = [
+    { exact: true, path: `/`, component: GettingStarted },
+    { exact: true, path: `getting-started`, component: GettingStarted },
+];
