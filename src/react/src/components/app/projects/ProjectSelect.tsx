b53669fbc2331551245366babd38bb4990ec2dc9
Nick Cromwell <cromwellnick@gmail.com>
2020-07-06 21:19:21 -0400

    Update project select

--- src/react/src/components/app/projects/ProjectSelect.tsx
+++ src/react/src/components/app/projects/ProjectSelect.tsx
@@ -1,53 +1,70 @@
 import * as React from 'react';
 import { selectProject } from '../../../redux/actions/SelecteProjectActions';
-import { Theme, createStyles, WithStyles, withStyles, Card, CardHeader, CardContent, Typography, CardMedia, Grid, ButtonBase } from '@material-ui/core';
+import { Theme, createStyles, WithStyles, withStyles, Card, CardHeader, CardContent, Typography, Grid, ButtonBase, IconButton } from '@material-ui/core';
+import ArrowLeft from 'mdi-material-ui/ArrowLeft';
 import { connect } from 'react-redux';
 import IProject from '../../../models/app/IProject';
 import { ApplicationState } from '../../../stores';
 import { push } from 'connected-react-router';
 import RoutePaths from '../../RoutePaths';
-const IntegrationApi = require('../../../../assets/integration-api.png');
 import * as queryString from 'query-string';
-import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import populateProjectsHOC from '../hocs/PopulateProjectsHOC';
 import { ProjectsState } from '../../../redux/actions/ProjectActions';
 import { resetGeofences } from '../../../redux/actions/GeofenceActions';
 import { resetIntegrations } from '../../../redux/actions/IntegrationActions';
+import classNames from 'classnames';
 
 const styles = (theme: Theme) =>
     createStyles({
+        layout: {
+            width: 'auto',
+            marginLeft: theme.spacing(2),
+            marginRight: theme.spacing(2),
+            marginTop: theme.spacing(3),
+            [theme.breakpoints.up(600 + theme.spacing(2 * 2))]: {
+                width: 600,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            },
+        },
+        root: {
+            display: 'flex',
+            minWidth: '100%',
+        },
+        details: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        content: {
+            flex: '1 0 auto',
+        },
+        cover: {
+            width: 151,
+        },
+        grid: {
+            padding: theme.spacing(2),
+        },
         card: {
             flexGrow: 1,
             height: '100%',
         },
-        buttons: {
-            display: 'flex',
-            justifyContent: 'flex-end',
-            paddingTop: 0,
+        title: {
+            marginTop: '0px',
+            padding: '0px',
         },
-        media: {
-            height: 0,
-            paddingTop: '96px',
-        },
-        layout: {
-            margin: theme.spacing(2),
-        },
-        parallaxContainer: {
-            position: 'absolute',
-            [theme.breakpoints.up('md')]: {
-                width: `calc(100% - ${theme.drawer.width}px - ${theme.spacing(4)}px)`,
-            },
-            height: '100%',
-            width: '100%',
+        return: {
+            position: 'sticky',
+            top: theme.toolbar.height + theme.spacing(4),
+            marginLeft: theme.spacing(4),
         },
         mediaRoot: {
             backgroundSize: 'contain',
         },
+        buttonBase: {
+            minWidth: '100%',
+        },
         rootPadding: {
             padding: theme.spacing(1),
-        },
-        gridButton: {
-            width: '100% !important',
         },
     });
 
@@ -91,7 +108,7 @@ class ProjectSelect extends React.Component<ProjectSelectProps> {
     }
 
     handleProjectClick(project: IProject) {
-        this.props.projectsState.projects.filter(a => a.name === project.name);
+        this.props.projectsState.projects.filter((a) => a.name === project.name);
         if (this.props.selectedProject?.projectId !== project.projectId) {
             this.props.selectProject(project);
             this.props.resetGeofences();
@@ -109,41 +126,36 @@ class ProjectSelect extends React.Component<ProjectSelectProps> {
         const { classes, projectsState } = this.props;
         return (
             <React.Fragment>
+                <IconButton className={classes.return} onClick={() => this.props.push(RoutePaths.Integrations)}>
+                    <ArrowLeft />
+                </IconButton>
+                <Typography className={classNames(classes.title, classes.layout)} variant="h5" align="left">
+                    Select A Project
+                </Typography>
                 <div className={classes.layout}>
-                    <Typography gutterBottom variant="h5" align="center">
-                        Which project would you like to view?
-                    </Typography>
-                    <div className={classes.parallaxContainer}>
-                        <Parallax pages={1} scrolling={true}>
-                            <ParallaxLayer speed={0.7}>
-                                <Grid container spacing={3} direction="row" justify="center" alignItems="baseline">
-                                    {projectsState.projects.map(project => (
-                                        <Grid key={project.name} item xs={12} sm={8} md={7}>
-                                            <ButtonBase
-                                                className={classes.gridButton}
-                                                onClick={() => {
-                                                    this.handleProjectClick(project);
-                                                }}
-                                            >
-                                                <Card elevation={3} className={classes.card}>
-                                                    <CardHeader
-                                                        titleTypographyProps={{ align: 'left' }}
-                                                        classes={{ root: classes.rootPadding }}
-                                                        title={project.name}
-                                                    />
-                                                    <CardContent classes={{ root: classes.rootPadding }}>
-                                                        <Typography align="left" component="p">
-                                                            {project.description}
-                                                        </Typography>
-                                                    </CardContent>
-                                                </Card>
-                                            </ButtonBase>
-                                        </Grid>
-                                    ))}
+                    <Grid container spacing={3} direction="column" justify="flex-start" alignItems="center">
+                        <Grid container item justify="center">
+                            {projectsState.projects.map((project) => (
+                                <Grid item xs={12}>
+                                    <ButtonBase
+                                        className={classes.buttonBase}
+                                        onClick={() => {
+                                            this.handleProjectClick(project);
+                                        }}
+                                    >
+                                        <Card elevation={4} className={classes.root}>
+                                            <CardHeader titleTypographyProps={{ align: 'left' }} classes={{ root: classes.rootPadding }} title={project.name} />
+                                            <CardContent classes={{ root: classes.rootPadding }}>
+                                                <Typography align="left" component="p">
+                                                    {project.description}
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </ButtonBase>
                                 </Grid>
-                            </ParallaxLayer>
-                        </Parallax>
-                    </div>
+                            ))}
+                        </Grid>
+                    </Grid>
                 </div>
             </React.Fragment>
         );
