b7fce4bb28a1e647117ab3aff11f2a5e8e73679a
Nick Cromwell <cromwellnick@gmail.com>
2020-07-07 23:00:00 -0400

    Subscription layout update

--- src/react/src/components/app/subscription/Subscription.tsx
+++ src/react/src/components/app/subscription/Subscription.tsx
@@ -132,129 +132,127 @@ class Subscription extends React.Component<SubscriptionProps, SubscriptionState>
         const { utilized, limit } = this.props.subscriptionLimitDetails;
         return (
             <React.Fragment>
-                <Grid container direction="column" justify="space-evenly" spacing={5}>
-                    <Grid container item justify="space-evenly" alignItems="center" spacing={5}>
-                        <Grid item xs={11} sm={8} md={5} lg={2}>
-                            <PlanCard
-                                isCurrentPlan={this.isCurrentPlan('sandbox')}
-                                planId="sandbox"
-                                planName="Sandbox"
-                                message="Perfect for getting a feel and testing your integrations"
-                                limitDetails={{ geofences: 100, accounts: 1, integrations: 2, projects: 1 } as ILimitDetails}
-                                cost="FREE"
-                                onUpgrade={this.upgrade.bind(this)}
-                                forceDisable={!this.props.subscriptionLimitDetails.active}
-                            />
-                        </Grid>
-                        <Grid item xs={11} sm={8} md={5} lg={2}>
-                            <PlanCard
-                                isCurrentPlan={this.isCurrentPlan('startup')}
-                                planId="startup"
-                                planName="Startup"
-                                message="When you're ready to start scaling and adding integrations"
-                                limitDetails={{ geofences: 1000, accounts: 3, integrations: 3, projects: 1 } as ILimitDetails}
-                                cost="$49 / Month"
-                                onUpgrade={this.upgrade.bind(this)}
-                                forceDisable={!this.props.subscriptionLimitDetails.active}
-                            />
-                        </Grid>
-                        <Grid item xs={11} sm={8} md={5} lg={2}>
-                            <PlanCard
-                                isCurrentPlan={this.isCurrentPlan('pro')}
-                                planId="pro"
-                                planName="Pro"
-                                message="Extend your reach with 5,000 geofences and 5 integrations"
-                                limitDetails={{ geofences: 5000, accounts: 5, integrations: 5, projects: 1 } as ILimitDetails}
-                                cost="$99 / Month"
-                                onUpgrade={this.upgrade.bind(this)}
-                                forceDisable={!this.props.subscriptionLimitDetails.active}
-                            />
-                        </Grid>
+                <Grid container item justify="space-evenly" alignItems="center" spacing={4}>
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            isCurrentPlan={this.isCurrentPlan('sandbox')}
+                            planId="sandbox"
+                            planName="Sandbox"
+                            message="Perfect for getting a feel and testing your integrations"
+                            limitDetails={{ geofences: 100, accounts: 1, integrations: 2, projects: 1 } as ILimitDetails}
+                            cost="FREE"
+                            onUpgrade={this.upgrade.bind(this)}
+                            forceDisable={!this.props.subscriptionLimitDetails.active}
+                        />
                     </Grid>
-                    <Grid container item justify="center">
-                        <Grid item xs={10}>
-                            <Paper className={classes.currentSubscription} elevation={3}>
-                                <Grid container>
-                                    <Hidden smDown>
-                                        <Grid md={2} item />
-                                    </Hidden>
-                                    <Grid md={8} xs={12} item>
-                                        <Typography align="center" variant="h6">
-                                            Your Subscription
-                                        </Typography>
-                                        <Typography align="center" color="primary" variant="h6">
-                                            {capitalCase(this.props.subscriptionLimitDetails.planId)}
-                                        </Typography>
-                                    </Grid>
-                                    <Grid md={2} xs={12} item>
-                                        <Grid container alignItems="flex-start" justify="flex-end" direction="row">
-                                            <Button color="primary" variant="outlined" onClick={this.openPortal.bind(this)} data-cb-type="portal">
-                                                Manage
-                                            </Button>
-                                        </Grid>
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            isCurrentPlan={this.isCurrentPlan('startup')}
+                            planId="startup"
+                            planName="Startup"
+                            message="When you're ready to start scaling and adding integrations"
+                            limitDetails={{ geofences: 1000, accounts: 3, integrations: 3, projects: 1 } as ILimitDetails}
+                            cost="$49 / Month"
+                            onUpgrade={this.upgrade.bind(this)}
+                            forceDisable={!this.props.subscriptionLimitDetails.active}
+                        />
+                    </Grid>
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            isCurrentPlan={this.isCurrentPlan('pro')}
+                            planId="pro"
+                            planName="Pro"
+                            message="Extend your reach with 5,000 geofences and 5 integrations"
+                            limitDetails={{ geofences: 5000, accounts: 5, integrations: 5, projects: 1 } as ILimitDetails}
+                            cost="$99 / Month"
+                            onUpgrade={this.upgrade.bind(this)}
+                            forceDisable={!this.props.subscriptionLimitDetails.active}
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container item justify="center">
+                    <Grid item xs={10}>
+                        <Paper className={classes.currentSubscription} elevation={3}>
+                            <Grid container>
+                                <Hidden smDown>
+                                    <Grid md={2} item />
+                                </Hidden>
+                                <Grid md={8} xs={12} item>
+                                    <Typography align="center" variant="h6">
+                                        Your Subscription
+                                    </Typography>
+                                    <Typography align="center" color="primary" variant="h6">
+                                        {capitalCase(this.props.subscriptionLimitDetails.planId)}
+                                    </Typography>
+                                </Grid>
+                                <Grid md={2} xs={12} item>
+                                    <Grid container alignItems="flex-start" justify="flex-end" direction="row">
+                                        <Button color="primary" variant="outlined" onClick={this.openPortal.bind(this)} data-cb-type="portal">
+                                            Manage
+                                        </Button>
                                     </Grid>
                                 </Grid>
+                            </Grid>
 
-                                <Grid container justify="space-around">
-                                    <Grid item>
-                                        <Typography variant="subtitle1">Geofences</Typography>
-                                        {this.isNearingLimit(utilized.geofences, limit.geofences) ? (
-                                            <Typography align="center" color="error" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.geofences)}/{this.numberWithCommas(limit.geofences)}
-                                            </Typography>
-                                        ) : (
-                                            <Typography align="center" color="primary" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.geofences)}/{this.numberWithCommas(limit.geofences)}
-                                            </Typography>
-                                        )}
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography variant="subtitle1">Integrations</Typography>
-                                        {this.isNearingLimit(utilized.integrations, limit.integrations) ? (
-                                            <Typography align="center" color="error" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.integrations)}/{this.numberWithCommas(limit.integrations)}
-                                            </Typography>
-                                        ) : (
-                                            <Typography align="center" color="primary" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.integrations)}/{this.numberWithCommas(limit.integrations)}
-                                            </Typography>
-                                        )}
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography variant="subtitle1">Projects</Typography>
-                                        {this.isNearingLimit(utilized.projects, limit.projects) ? (
-                                            <Typography align="center" color="error" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.projects)}/{this.numberWithCommas(limit.projects)}
-                                            </Typography>
-                                        ) : (
-                                            <Typography align="center" color="primary" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.projects)}/{this.numberWithCommas(limit.projects)}
-                                            </Typography>
-                                        )}
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography variant="subtitle1">User Accounts</Typography>
-                                        {this.isNearingLimit(utilized.accounts, limit.accounts) ? (
-                                            <Typography align="center" color="error" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.accounts)}/{this.numberWithCommas(limit.accounts)}
-                                            </Typography>
-                                        ) : (
-                                            <Typography align="center" color="primary" variant="subtitle1">
-                                                {this.numberWithCommas(utilized.accounts)}/{this.numberWithCommas(limit.accounts)}
-                                            </Typography>
-                                        )}
-                                    </Grid>
+                            <Grid container justify="space-around">
+                                <Grid item>
+                                    <Typography variant="subtitle1">Geofences</Typography>
+                                    {this.isNearingLimit(utilized.geofences, limit.geofences) ? (
+                                        <Typography align="center" color="error" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.geofences)}/{this.numberWithCommas(limit.geofences)}
+                                        </Typography>
+                                    ) : (
+                                        <Typography align="center" color="primary" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.geofences)}/{this.numberWithCommas(limit.geofences)}
+                                        </Typography>
+                                    )}
                                 </Grid>
-                            </Paper>
-                        </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1">Integrations</Typography>
+                                    {this.isNearingLimit(utilized.integrations, limit.integrations) ? (
+                                        <Typography align="center" color="error" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.integrations)}/{this.numberWithCommas(limit.integrations)}
+                                        </Typography>
+                                    ) : (
+                                        <Typography align="center" color="primary" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.integrations)}/{this.numberWithCommas(limit.integrations)}
+                                        </Typography>
+                                    )}
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1">Projects</Typography>
+                                    {this.isNearingLimit(utilized.projects, limit.projects) ? (
+                                        <Typography align="center" color="error" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.projects)}/{this.numberWithCommas(limit.projects)}
+                                        </Typography>
+                                    ) : (
+                                        <Typography align="center" color="primary" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.projects)}/{this.numberWithCommas(limit.projects)}
+                                        </Typography>
+                                    )}
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1">User Accounts</Typography>
+                                    {this.isNearingLimit(utilized.accounts, limit.accounts) ? (
+                                        <Typography align="center" color="error" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.accounts)}/{this.numberWithCommas(limit.accounts)}
+                                        </Typography>
+                                    ) : (
+                                        <Typography align="center" color="primary" variant="subtitle1">
+                                            {this.numberWithCommas(utilized.accounts)}/{this.numberWithCommas(limit.accounts)}
+                                        </Typography>
+                                    )}
+                                </Grid>
+                            </Grid>
+                        </Paper>
                     </Grid>
+                </Grid>
+                <Grid container item justify="center" alignContent="center">
                     <Grid container item justify="center" alignContent="center">
-                        <Grid container item justify="center" alignContent="center">
-                            <Typography align="center" variant="caption">
-                                Please note, when downgrading your subscription resources may need to be removed. To understand how resources are removed when
-                                downgrading, please refer to the documentation <Link href={RoutePaths.Docs.replace(':name?', '')}>here</Link>.
-                            </Typography>
-                        </Grid>
+                        <Typography align="center" variant="caption">
+                            Please note, when downgrading your subscription resources may need to be removed. To understand how resources are removed when
+                            downgrading, please refer to the documentation <Link href={RoutePaths.Docs.replace(':name?', '')}>here</Link>.
+                        </Typography>
                     </Grid>
                 </Grid>
             </React.Fragment>
