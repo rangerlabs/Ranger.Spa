c6d9ecb2d7674dc274b5385d498d8af37ed9df17
Nick Cromwell <cromwellnick@gmail.com>
2020-07-08 00:10:03 -0400

    Pricing page

---
+++ src/react/src/components/landing/pricing/PlanCard.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Box, Button, Grid } from '@material-ui/core';
+import Constants from '../../../theme/Constants';
+import { ILimitDetails } from '../../../models/app/ILimitDetails';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            maxWidth: 345,
+            padding: theme.spacing(2),
+        },
+        header: {
+            textAlign: 'center',
+        },
+        pricing: {
+            maxHeight: theme.spacing(4),
+            minHeight: theme.spacing(4),
+            paddingTop: theme.spacing(1),
+        },
+        headerFaded: {
+            textAlign: 'center',
+        },
+        buttons: {
+            display: 'flex',
+            justifyContent: 'center',
+            padding: theme.spacing(2),
+        },
+        white: {
+            color: Constants.COLORS.WHITE,
+        },
+        cardContent: {
+            paddingTop: 0,
+        },
+        message: {
+            maxHeight: '66px',
+            minHeight: '66px',
+        },
+    })
+);
+
+interface PlanCardProps {
+    planName: string;
+    cost: string;
+    message: string;
+    limitDetails: ILimitDetails;
+}
+
+export default function PlanCard(props: PlanCardProps) {
+    const classes = useStyles(props);
+
+    function numberWithCommas(x: number): string {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    return (
+        <Card className={classes.root} elevation={3}>
+            <Box className={classes.header}>
+                <Typography color="primary" align="center" variant="h6">
+                    {props.planName}
+                </Typography>
+                <Box paddingTop={1} paddingBottom={1} display="inline-block" width="90%" borderBottom="1px solid lightgray"></Box>
+                <Typography className={classes.pricing} color="primary" align="center" variant="h5">
+                    {props.cost}
+                </Typography>
+                <Box paddingBottom={1} display="inline-block" width="90%" borderBottom="1px solid lightgray"></Box>
+            </Box>
+            <CardContent className={classes.cardContent}>
+                <Grid container justify="space-between" className={classes.message}>
+                    <Typography variant="subtitle1" align="center">
+                        {props.message}
+                    </Typography>
+                </Grid>
+                <Box paddingTop={1} marginBottom={1} display="inline-block" width="90%" borderBottom="1px solid lightgray"></Box>
+                <Grid container justify="space-between">
+                    <Grid>
+                        <Typography variant="body2">Geofences</Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="body2">{props.limitDetails.geofences ? numberWithCommas(props.limitDetails.geofences) : '∞'}</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container justify="space-between">
+                    <Grid>
+                        <Typography variant="body2">Integrations</Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="body2">{props.limitDetails.integrations ? numberWithCommas(props.limitDetails.integrations) : '∞'}</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container justify="space-between">
+                    <Grid>
+                        <Typography variant="body2">Projects</Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="body2">{props.limitDetails.projects ? numberWithCommas(props.limitDetails.projects) : '∞'}</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container justify="space-between">
+                    <Grid>
+                        <Typography variant="body2">User Accounts</Typography>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="body2">{props.limitDetails.accounts ? numberWithCommas(props.limitDetails.accounts) : '∞'}</Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
+}
--- src/react/src/components/landing/pricing/Pricing.tsx
+++ src/react/src/components/landing/pricing/Pricing.tsx
@@ -1,7 +1,61 @@
-import * as React from "react";
+import * as React from 'react';
+import { Grid, createStyles, Theme, makeStyles, Typography } from '@material-ui/core';
+import PlanCard from './PlanCard';
+import { ILimitDetails } from '../../../models/app/ILimitDetails';
+import Footer from '../footer/Footer';
 
-export default class Pricing extends React.Component {
-    render(): any {
-        return null;
-    }
-}
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        height100: {
+            height: '100%',
+        },
+    })
+);
+
+interface PricingProps {}
+
+const Pricing = function (props: PricingProps) {
+    const classes = useStyles(props);
+
+    return (
+        <React.Fragment>
+            <Grid className={classes.height100} container direction="column">
+                <Grid container item justify="center">
+                    <Grid item>
+                        <Typography variant="h3">Flexible Plans To Meet Your Needs</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h5">Enterprise plans coming soon</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container item justify="space-evenly" alignItems="center">
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            planName="Sandbox"
+                            message="Perfect for getting a feel and testing your integrations"
+                            limitDetails={{ geofences: 100, accounts: 1, integrations: 2, projects: 1 } as ILimitDetails}
+                            cost="FREE"
+                        />
+                    </Grid>
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            planName="Startup"
+                            message="When you're ready to start scaling and adding integrations"
+                            limitDetails={{ geofences: 1000, accounts: 3, integrations: 3, projects: 1 } as ILimitDetails}
+                            cost="$49 / Month"
+                        />
+                    </Grid>
+                    <Grid item xs={11} sm={8} md={5} lg={2}>
+                        <PlanCard
+                            planName="Pro"
+                            message="Extend your reach with 5,000 geofences and 5 integrations"
+                            limitDetails={{ geofences: 5000, accounts: 5, integrations: 5, projects: 1 } as ILimitDetails}
+                            cost="$99 / Month"
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Footer />
+        </React.Fragment>
+    );
+};
