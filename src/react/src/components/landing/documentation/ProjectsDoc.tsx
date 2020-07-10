a5f98b90b4ee46d8a7ef1a7185a9fd3a163d30cd
Nick Cromwell <cromwellnick@gmail.com>
2020-07-10 00:01:17 -0400

    Projects doc update

--- src/react/src/components/landing/documentation/ProjectsDoc.tsx
+++ src/react/src/components/landing/documentation/ProjectsDoc.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Typography, createStyles, Theme, WithStyles, withStyles } from '@material-ui/core';
+import { Typography, createStyles, Theme, WithStyles, withStyles, Paper, Box, List, ListItem, ListItemText } from '@material-ui/core';
+import NewApiKeys from '../../../../assets/new-api-keys.png';
 
 const styles = (theme: Theme) =>
     createStyles({
@@ -30,12 +31,56 @@ const ProjectsDoc = function (props: ProjectsDocProps) {
                 Creating a New Project
             </Typography>
             <div className={classes.blockText}>
-                <Typography gutterBottom variant="body1"></Typography>
-                <Typography gutterBottom variant="body1"></Typography>
+                <Typography gutterBottom variant="body1">
+                    Creating a project will be the first suggested action in Ranger. Doing so allows access to the Integrations and Geofences sections of the
+                    platform. Select a unique name an optional description for the project.
+                </Typography>
             </div>
             {/* <div className={classes.imgContainer}>
-                <img style={{ maxWidth: '70%' }} src={''} alt="central-park-geofence" />
+                <img style={{ maxWidth: '70%' }} src={ProjectNew} alt="New Project" />
             </div> */}
+            <div className={classes.blockText}>
+                <Typography gutterBottom variant="body1">
+                    Once the project is created you will be presented with three API keys.{' '}
+                    <Box display="inline" fontWeight="fontWeightBold">
+                        <Typography display="inline" variant="body1">
+                            Store these API keys securely. They cannot be recovered.
+                        </Typography>
+                    </Box>
+                    API keys can only be re-generated.
+                </Typography>
+            </div>
+            <div className={classes.imgContainer}>
+                <img style={{ maxWidth: '70%' }} src={NewApiKeys} alt="New API Keys" />
+            </div>
+            <div className={classes.blockText}>
+                <Typography gutterBottom variant="body1">
+                    These API Keys represent distinct purposes within Ranger as described below:
+                </Typography>
+                <List>
+                    <ListItem>
+                        <ListItemText primary="Live API Key" />
+                        <ListItemText
+                            inset
+                            primary="The Live API Key is intended to be deployed into a production environment and will execute all Integrations whose environent is set to LIVE."
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Test API Key" />
+                        <ListItemText
+                            inset
+                            primary="The Test API Key is intended to be deployed to any non-production environment and will execute all Integrations whose environent is set to TEST."
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Project API Key" />
+                        <ListItemText
+                            inset
+                            primary="The Project API Key provides the ability to programatically manage a subset of resources within a project. For Example, this key can be used CRUD operations on Geofence resources."
+                        />
+                    </ListItem>
+                </List>
+            </div>
         </React.Fragment>
     );
 };
