0fca09abd2c3ea67df93c5411bc87de80faf4856
Nick Cromwell <cromwellnick@gmail.com>
2020-07-12 19:40:23 -0400

    Update outline

--- src/react/src/components/landing/documentation/IntegrationsDocs/IntegrationsDoc.tsx
+++ src/react/src/components/landing/documentation/IntegrationsDocs/IntegrationsDoc.tsx
@@ -31,28 +31,34 @@ const IntegrationsDoc = function (props: IntegrationsDocProps) {
             </Typography>
             <div className={classes.blockText}>
                 <Typography className={classes.blockText} variant="body1">
-                    Integrations enables Ranger to communicate to the systems your organiation and services depend on. Ranger's platform was built to be
-                    extended and more integrations will come as our team learns our customer's needs. If you would like to request that Ranger integrate a
-                    platform, drop us a line at <Link href="mailto:info@rangerlabs.io">info@rangerlabs.io</Link>.
+                    Integrations enable Ranger to communicate to the systems your organiation and services depend on. Ranger's platform was built to be extended
+                    and more integrations will come as our team learns our customer's needs. If you would like to request that Ranger integrate with a
+                    particular service, send a recommendation to<Link href="mailto:info@rangerlabs.io">info@rangerlabs.io</Link>.
                 </Typography>
             </div>
             <Typography className={classes.blockText} variant="h5">
                 Outline
             </Typography>
             <div className={classes.blockText}>
-                <List>
-                    <ListItem
-                        classes={{ container: classes.listItem }}
-                        onClick={() => {
-                            document.getElementById('webhook-section').scrollIntoView({ behavior: 'smooth' });
-                        }}
-                    >
-                        <ListItemIcon>
+                <List dense>
+                    <ListItem>
+                        <ListItemIcon classes={{ root: classes.listItem }}>
                             <Box fontSize=".65rem">
                                 <RadioButtonUncheckedIcon fontSize="inherit" />
                             </Box>
                         </ListItemIcon>
-                        <ListItemText primaryTypographyProps={{ component: 'a' }} primary="Webhooks" />
+                        <ListItemText
+                            disableTypography
+                            primary={
+                                <Link
+                                    onClick={() => {
+                                        document.getElementById('webhook-section').scrollIntoView({ behavior: 'smooth' });
+                                    }}
+                                >
+                                    Webhooks
+                                </Link>
+                            }
+                        />
                     </ListItem>
                 </List>
             </div>
