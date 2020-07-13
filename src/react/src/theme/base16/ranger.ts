286a268abb3e0afce05cc99ac8b50024166b4c50
Nick Cromwell <cromwellnick@gmail.com>
2020-07-13 11:46:14 -0400

    Update jsonViewer theme

--- src/react/src/theme/base16/ranger.ts
+++ src/react/src/theme/base16/ranger.ts
@@ -10,7 +10,7 @@ const jsonViewerTheme = {
     base06: '252525',
     base07: '101010',
     base08: '7c7c7c',
-    base09: '999999',
+    base09: `${Constants.COLORS.PRIMARY_COLOR}`.replace('#', ''),
     base0A: 'a0a0a0',
     base0B: '8e8e8e',
     base0C: '868686',
