26ec27ffa58fb7a2f708b990aaa117e0e9166cc7
Nick Cromwell <cromwellnick@gmail.com>
2020-07-17 12:15:47 -0400

    set px for iframe

--- src/react/src/components/landing/newsletter/Newsletter.tsx
+++ src/react/src/components/landing/newsletter/Newsletter.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 export default function Newsletter() {
     return (
-        <React.Fragment>
-            <iframe
-                style={{ width: '100%', height: '100%', border: 'none' }}
-                src="https://cdn.forms-content.sg-form.com/3dcc4eed-c7d3-11ea-89e1-567c2b280faf"
-            />
-        </React.Fragment>
+        <iframe style={{ width: '100%', height: '296px', border: 'none' }} src="https://cdn.forms-content.sg-form.com/3dcc4eed-c7d3-11ea-89e1-567c2b280faf" />
     );
 }
