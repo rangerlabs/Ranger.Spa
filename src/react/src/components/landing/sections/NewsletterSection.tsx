d20da4836f9c43c085fac5e1b4b414acc590f0f3
Nick Cromwell <cromwellnick@gmail.com>
2020-07-16 23:26:22 -0400

    newsletter section

--- src/react/src/components/landing/sections/NewsletterSection.tsx
+++ src/react/src/components/landing/sections/NewsletterSection.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Theme, createStyles, Grid, Paper } from '@material-ui/core';
+import { Theme, createStyles, Grid, Paper, Typography } from '@material-ui/core';
 import Newsletter from '../newsletter/Newsletter';
 import { makeStyles } from '@material-ui/styles';
 
@@ -8,6 +8,9 @@ const useStyles = makeStyles((theme: Theme) =>
         background: {
             backgroundColor: theme.palette.primary.main,
         },
+        typography: {
+            color: theme.palette.common.white,
+        },
     })
 );
 
@@ -15,6 +18,16 @@ export default function NewsletterSection() {
     const classes = useStyles();
     return (
         <Grid className={classes.background} container alignContent="center" justify="center" spacing={5}>
+            <Grid item xs={12}>
+                <Typography gutterBottom className={classes.typography} variant="h3">
+                    Ranger Newsletter
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography gutterBottom className={classes.typography} variant="subtitle1">
+                    Be the first to hear all the latest Ranger news and features.
+                </Typography>
+            </Grid>
             <Grid item md={5} xs={12}>
                 <Paper elevation={3}>
                     <Newsletter />
