d3c656d59baa9c0bb47bd5d6169d769a41f038e4
Nick Cromwell <cromwellnick@gmail.com>
2020-07-17 11:41:21 -0400

    Remove grid layout

--- src/react/src/components/landing/sections/NewsletterSection.tsx
+++ src/react/src/components/landing/sections/NewsletterSection.tsx
@@ -5,8 +5,17 @@ import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        background: {
-            backgroundColor: theme.palette.primary.main,
+        layout: {
+            padding: theme.spacing(4),
+            width: 'auto',
+            marginTop: theme.toolbar.height,
+            marginLeft: theme.spacing(2),
+            marginRight: theme.spacing(2),
+            [theme.breakpoints.up(600 + theme.spacing(2 * 2))]: {
+                width: 600,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+            },
         },
         typography: {
             color: theme.palette.common.white,
@@ -20,18 +29,18 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function NewsletterSection() {
     const classes = useStyles();
     return (
-        <Grid id="newsletter-section" className={classes.background} container alignContent="center" justify="center" spacing={5}>
-            <Grid item md={5} xs={12}>
-                <div>
-                    <Typography align="center" gutterBottom className={classes.typography} variant="h3">
-                        Ranger Newsletter
-                    </Typography>
-                    <Typography align="center" gutterBottom className={classes.typography} variant="subtitle1">
-                        Be the first to hear the latest in Ranger news and features.
-                    </Typography>
-                </div>
+        <div id="newsletter-section" className={classes.layout}>
+            <div>
+                <Typography align="center" gutterBottom className={classes.typography} variant="h3">
+                    Ranger Newsletter
+                </Typography>
+                <Typography align="center" gutterBottom className={classes.typography} variant="subtitle1">
+                    Be the first to hear the latest in Ranger news and features.
+                </Typography>
+            </div>
+            <div>
                 <Newsletter />
-            </Grid>
-        </Grid>
+            </div>
+        </div>
     );
 }
