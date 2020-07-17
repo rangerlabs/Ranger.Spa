9b940a294d6614ff0f1e0ddd260c30220220c27e
Nick Cromwell <cromwellnick@gmail.com>
2020-07-17 14:31:25 -0400

    newsletter spacing

--- src/react/src/components/landing/sections/NewsletterSection.tsx
+++ src/react/src/components/landing/sections/NewsletterSection.tsx
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function NewsletterSection() {
     const classes = useStyles();
     return (
-        <Grid id="newsletter-section" className={classes.background} container alignContent="center" justify="center">
+        <Grid id="newsletter-section" className={classes.background} container alignContent="center" justify="center" spacing={5}>
             <Grid item>
                 <div>
                     <Typography align="center" gutterBottom className={classes.typography} variant="h3">
