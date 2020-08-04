a679fc2650e714062f80b66a8c133de35807c196
Nick Cromwell <cromwellnick@gmail.com>
2020-08-04 13:45:57 -0400

    Added my bio

--- src/react/src/components/landing/about/About.tsx
+++ src/react/src/components/landing/about/About.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Grid, createStyles, Theme, makeStyles, Typography, Paper, Button, Box } from '@material-ui/core';
+import { Grid, createStyles, Theme, makeStyles, Typography, Box, useMediaQuery, useTheme } from '@material-ui/core';
 import Footer from '../footer/Footer';
 import NameLogo from '../../../theme/NameLogo';
 import Paragraph from '../documentation/docComponents/Paragraph';
 import Block from '../documentation/docComponents/Block';
+import CromwellProfile from '../../../../assets/cromwell-profile.jpg';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -32,6 +33,9 @@ interface AboutProps {}
 
 export default function About(props: AboutProps) {
     const classes = useStyles(props);
+    const theme = useTheme();
+    const isMdDown = useMediaQuery(theme.breakpoints.down('md'));
+    const isSmDown = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
         <React.Fragment>
@@ -39,14 +43,14 @@ export default function About(props: AboutProps) {
                 <Grid container item justify="center" spacing={5}>
                     <Grid item xs={12}>
                         <Box textAlign="center">
-                            <NameLogo width="15%" />
+                            <NameLogo width={isMdDown ? '25%' : isSmDown ? '50%' : '15%'} />
                         </Box>
                     </Grid>
                 </Grid>
                 <Grid container item justify="center">
-                    <Grid item xs={6}>
+                    <Grid item md={6} xs={11}>
                         <Block>
-                            <Typography gutterBottom variant="h6">
+                            <Typography style={{ fontWeight: 400 }} gutterBottom variant="h6">
                                 Ranger was built to be the most extendable, configurable, and easy-to-use location platform available. With Ranger, mobile apps
                                 become actionable when it matters most to you and your users.
                             </Typography>
@@ -68,16 +72,24 @@ export default function About(props: AboutProps) {
                     </Grid>
                 </Grid>
                 <Grid container item justify="center">
-                    <Grid item xs={2}>
-                        <Paragraph></Paragraph>
+                    <Grid item md={2} xs={4}>
+                        <Block>
+                            <img src={CromwellProfile} alt="Nick Cromwell Image" />
+                        </Block>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Paragraph>
-                            The advent of smart devices has led to an overwhelming volume of location data. Ranger’s SaaS infrastructure enables our customers
-                            to cut through the noise and take action when and where they need. We provide a modern solution to a modern problem - and to do
-                            this, we’ve built Ranger on today’s most bleeding-edge, cloud-native technologies. For our customers, this means reliability and
-                            scalability for their business critical apps.
-                        </Paragraph>
+                    <Grid item md={4} xs={8}>
+                        <Typography variant="h6">Founder, Chief Architect</Typography>
+
+                        <Block>
+                            <Typography gutterBottom variant="subtitle1">
+                                Nick Cromwell is the Founder and Chief Architect of Ranger. He has a decade of experience delivering scalable, distributed
+                                architectures to market. He holds a BS degree in Applied Math from Kent State University and takes pride in his hometown of
+                                Cleveland, Ohio.
+                            </Typography>
+                            <Typography gutterBottom>
+                                Nick's passion for architecting Ranger was founded out of necessity when he set out to design a mobile app.
+                            </Typography>
+                        </Block>
                     </Grid>
                 </Grid>
             </Grid>
