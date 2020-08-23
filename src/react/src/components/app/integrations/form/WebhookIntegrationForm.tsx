2aee42262a0f32a6e44fb6165dc126eb92849da4
Nick Cromwell <cromwellnick@gmail.com>
2020-08-22 22:54:24 -0400

    wording

--- src/react/src/components/app/integrations/form/WebhookIntegrationForm.tsx
+++ src/react/src/components/app/integrations/form/WebhookIntegrationForm.tsx
@@ -207,7 +207,7 @@ class WebhookIntegrationForm extends React.Component<IWebhookIntegrationFormProp
                                     </Grid>
                                     <Grid item xs={12}>
                                         <FormikCheckbox
-                                            infoText="Select to execute this integration for events on all geofences"
+                                            infoText="Execute this integration for events on all geofences within this project"
                                             name="isDefault"
                                             label="Default Integration"
                                             value={props.values.isDefault}
