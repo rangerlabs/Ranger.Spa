d6a11be5eac7fbd9a1e2ddc25490ebd9842378d9
Nick Cromwell <cromwellnick@gmail.com>
2020-08-23 21:59:05 -0400

    Fix state assignment

--- src/react/src/redux/reducers/AccountReducer.ts
+++ src/react/src/redux/reducers/AccountReducer.ts
@@ -3,7 +3,7 @@ import { AccountAction, SET_DELETING_ACCOUNT } from '../actions/AccountActions';
 export function accountReducer(state = { isDeleting: false }, action: AccountAction) {
     switch (action.type) {
         case SET_DELETING_ACCOUNT: {
-            return Object.assign({}, state, { isDeleting: state.isDeleting });
+            return Object.assign({}, state, { isDeleting: action.accountState.isDeleting });
         }
         default:
             return state;
