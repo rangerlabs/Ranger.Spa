4166a08e503a5e5559efeba2f16d021655cb7219
Nick Cromwell <nick.cromwell@rangerlabs.io>
2020-11-12 00:00:06 -0500

    Update debounceNonDistinct

--- src/react/src/rxjs/debounceNonDistinct.ts
+++ src/react/src/rxjs/debounceNonDistinct.ts
@@ -1,5 +1,5 @@
 import { Observable, timer, empty, Subscriber, Subject } from 'rxjs';
-import { debounce } from 'rxjs/operators';
+import { debounce, takeLast } from 'rxjs/operators';
 
 declare module 'rxjs' {
     interface Observable<T> {
@@ -12,28 +12,19 @@ export default function PrototypeExtensions() {
         const source$: Observable<T> = this;
 
         return new Observable<T>((observer) => {
-            // Using an object as the default value
-            // so that the first time we check it
-            // if its the same its guaranteed to be false
-            // because every object has a different identity.
-            // Can't use null or undefined because source may
-            // emit these!
             let lastSeen = {};
 
             return source$
                 .pipe(
                     debounce((value: T) => {
-                        // If the last value has the same identity we'll
-                        // actually debounce
                         if (value !== lastSeen) {
                             lastSeen = value;
                             return timer(delay);
                         } else {
-                            // This will complete() right away so we don't actually debounce/buffer
-                            // it at all
                             return empty();
                         }
-                    })
+                    }),
+                    takeLast(1)
                 )
                 .subscribe(observer);
         });
