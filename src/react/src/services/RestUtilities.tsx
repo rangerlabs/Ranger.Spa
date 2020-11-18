d95923be74a82ba9a6b8624c63a84b3194d7d0f0
Nick Cromwell <nick.cromwell@rangerlabs.io>
2020-11-18 01:33:52 -0500

    Assign headers

--- src/react/src/services/RestUtilities.tsx
+++ src/react/src/services/RestUtilities.tsx
@@ -127,7 +127,7 @@ export default class RestUtilities {
                 isError = response.status === 304 || (response.status >= 400 && response.status <= 500) ? true : false;
                 correlationId = response.headers.has('x-operation') ? response.headers.get('x-operation').replace('operations/', '') : null;
                 statusCode = response.status;
-                responseHeaders = response.headers;
+                responseHeaders = Object.assign({}, response.headers) as Headers;
                 return response.text();
             })
             .then((responseContent: string) => {
