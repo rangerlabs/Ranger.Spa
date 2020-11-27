55791ba94a73fe73f42275e3c720599fad2b7b6b
Nick Cromwell <nick.cromwell@rangerlabs.io>
2020-11-27 15:08:41 -0500

    Fix delete button

--- src/react/src/components/form/FormikDeleteButton.tsx
+++ src/react/src/components/form/FormikDeleteButton.tsx
@@ -3,12 +3,14 @@ import { ButtonProps } from '@material-ui/core/Button';
 import { openDialog, DialogContent } from '../../redux/actions/DialogActions';
 import { connect } from 'react-redux';
 import FormikDestructiveButton from './FormikDestructiveButton';
+import { PropsWithChildren } from 'react';
 
 interface FormikDeleteButtonProps {
     isSubmitting: boolean;
     dialogTitle: string;
     dialogContent?: JSX.Element | string;
     confirmText?: string;
+    disabled: boolean;
     onConfirm?: Function;
     onCancel?: Function;
     openDialog: (dialogContent: DialogContent) => void;
@@ -23,7 +25,7 @@ const mapDispatchToProps = (dispatch: any) => {
     };
 };
 
-class FormikDeleteButton extends React.Component<FormikDeleteButtonProps> {
+class FormikDeleteButton extends React.Component<PropsWithChildren<FormikDeleteButtonProps>> {
     renderDialog = () => {
         const { dialogTitle, dialogContent, confirmText, onConfirm, onCancel } = this.props;
         this.props.openDialog(new DialogContent(dialogContent, dialogTitle, confirmText, onConfirm, onCancel));
