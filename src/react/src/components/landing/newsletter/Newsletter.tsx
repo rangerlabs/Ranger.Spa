4bb8a4f357bc93f3239f90c17662ee70877fc13f
Nick Cromwell <nick.cromwell@rangerlabs.io>
2021-01-21 10:16:15 -0500

    Update Newsletter.tsx
    Set newsletter height to 300px

--- src/react/src/components/landing/newsletter/Newsletter.tsx
+++ src/react/src/components/landing/newsletter/Newsletter.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Newsletter() {
     return (
-        <iframe style={{ width: '100%', height: '296px', border: 'none' }} src="https://cdn.forms-content.sg-form.com/3dcc4eed-c7d3-11ea-89e1-567c2b280faf" />
+        <iframe style={{ width: '100%', height: '300px', border: 'none' }} src="https://cdn.forms-content.sg-form.com/3dcc4eed-c7d3-11ea-89e1-567c2b280faf" />
     );
 }
